91324228c2e07b1273be5805ca6f2d514a22f09f
Carl Björknäs <carl.bjorknas@gmail.com>
2021-09-19 13:08:25 +0200

    Moved the C# code from the HomePage component to a separate class.

---
+++ BlazingTrails.Client/Features/Home/HomePage.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Client.Features.Home
+{
+    public partial class HomePage : ComponentBase
+    {
+        private IEnumerable<Trail> _trails;
+
+        [Inject] public HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                _trails = await Http.GetFromJsonAsync<IEnumerable<Trail>>("trails/trail-data.json");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"There was a problem loading trail data: {ex.Message}");
+            }
+        }
+    }
+}
