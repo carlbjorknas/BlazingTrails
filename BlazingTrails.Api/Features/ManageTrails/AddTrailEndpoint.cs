4dbc52bebb6d5bd25447ad9fcd95b05d27ec93b5
Carl Björknäs <carl.bjorknas@gmail.com>
2021-10-02 17:38:47 +0200

    Enables saving of new trail using the message library MediatR and Ardalis ApiEndpoints.

---
+++ BlazingTrails.Api/Features/ManageTrails/AddTrailEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using Ardalis.ApiEndpoints;
+using BlazingTrails.Api.Persistence;
+using BlazingTrails.Api.Persistence.Entities;
+using BlazingTrails.Shared.Features.ManageTrails;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Api.Features.ManageTrails
+{
+    public class AddTrailEndpoint : BaseAsyncEndpoint
+        .WithRequest<AddTrailRequest>
+        .WithResponse<int>
+    {
+        private readonly BlazingTrailsContext _database;
+
+        public AddTrailEndpoint(BlazingTrailsContext database)
+        {
+            _database = database;
+        }
+
+        [HttpPost(AddTrailRequest.RouteTemplate)]
+        public override async Task<ActionResult<int>> HandleAsync(AddTrailRequest request, CancellationToken cancellationToken = default)
+        {
+            var trail = new Trail
+            {
+                Name = request.Trail.Name,
+                Description = request.Trail.Description,
+                Image = "",
+                Location = request.Trail.Location,
+                TimeInMinutes = request.Trail.TimeInMinutes,
+                Length = request.Trail.Length,
+                IsFavourite = false
+            };
+            await _database.Trails.AddAsync(trail, cancellationToken);
+
+            var routeInstructions = request.Trail.Route.Select(_ => new RouteInstruction
+            {
+                Stage = _.Stage,
+                Description = _.Description,
+                Trail = trail
+            });
+
+            await _database.RouteInstructions.AddRangeAsync(routeInstructions, cancellationToken);
+            await _database.SaveChangesAsync(cancellationToken);
+
+            return Ok(trail.Id);
+        }
+    }
+}
--- BlazingTrails.Client/Program.cs
+++ BlazingTrails.Client/Program.cs
@@ -1,11 +1,8 @@
+using MediatR;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazingTrails.Client
@@ -18,6 +15,7 @@ namespace BlazingTrails.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMediatR(typeof(Program).Assembly);
 
             await builder.Build().RunAsync();
         }
